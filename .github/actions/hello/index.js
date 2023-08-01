f0bb9454687ed99da17e67552810b4613ddd0854
Falk Raphael Wuttke <falk-raphael.wuttke@adesso.de>
2023-08-01 16:09:56 +0200

    style: formatted

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,12 +2,12 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  core.debug('Debugging action');
+  core.debug("Debugging action");
   const name = core.getInput("who-to-greet");
   console.log(`Hello ${name}`);
   const time = new Date();
   core.setOutput("time", time.toTimeString());
-  core.startGroup('github');
+  core.startGroup("github");
   console.log(JSON.stringify(github, null, "\t"));
   core.endGroup();
 } catch (e) {
